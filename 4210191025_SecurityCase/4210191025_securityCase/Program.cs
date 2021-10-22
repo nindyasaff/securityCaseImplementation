7d4eff85a7e9037033fb4548addbc14fa0c36aff
nindyasaff <63599760+nindyasaff@users.noreply.github.com>
2021-10-22 12:04:04 +0700

    Add files via upload

---
+++ 4210191025_SecurityCase/4210191025_securityCase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4210191025_securityCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
